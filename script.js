4aaa1fde713c4ef2e5af3176c61aeb9d6f662643
dhivya240211 <dhivya24dec@gmail.com>
2024-08-16 12:13:06 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,31 @@
+// const numb=[1,2,3,4]
+// const newArray=numb.map((a)=>{
+//     return a*5
+// })
+// console.log(newArray);
+// const ages=[10,2,30,21]
+// const newAge=ages.filter((a)=>{
+//     return a>2
+// })
+// console.log(newAge);
+// const marks=[20,39,40,1]
+// const total=marks.reduce((total,cv)=>{
+//     return total+cv
+// })
+// console.log(total);
+// console.log("first");
+// function greeting(){
+//     console.log("good morning");
+// }
+// setTimeout(greeting,3000)
+// console.log("last");
+// let arr=[];
+// arr[1]="apple";
+// arr[2]="name";
+// console.log(arr);
+//  Array=[1,2,3,4,5]
+// let res=Array.slice(1,4)
+// console.log(res);
+let sed=[1,2,3,4,5];
+sed=[];
+console.log(sed);
